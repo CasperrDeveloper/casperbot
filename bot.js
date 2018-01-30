cb6b18f3934548eda3a7531640b451db67f04adc
PumpkinDeveloper <32527786+PumpkinDeveloper@users.noreply.github.com>
2018-01-29 20:28:15 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 const config = require("./config.json");
-const fs = require("fs")
 
-
-client.on('message', msg => {
+client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
 
-  //define args
+  // This is the best way to define args. Trust me.
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+  // The list of if/else is replaced with those simple 2 lines:
   try {
     let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
+    if (!commandFile) return;
+    commandFile.run(client, message, args, config);
   } catch (err) {
     console.error(err);
   }
